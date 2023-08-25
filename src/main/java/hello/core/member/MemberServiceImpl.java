ec8bec7d01b5144c65f39c30aa6511ae7df25659
DAHEEKIM1 <66730012+DAHEEKIM1@users.noreply.github.com>
2023-08-25 22:50:25 +0900

    Section3 ing

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,13 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();//할당하는 부분이 구현체를 의존하는 것. (DIP를 위반하는 것)
+    private final MemberRepository memberRepository;//할당하는 부분이 구현체를 의존하는 것. (DIP를 위반하는 것)
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+        //생성자를 통해서 이 memberRepository 구현체에 뭐가 들어갈지를
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,24 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 //메모리 repository와 고정 할인 정책을 구현체로 만들어서 사용하고 있다.
-    private final MemberRepository memberRepository=new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy=new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+   // private final DiscountPolicy discountPolicy1=new RateDiscountPolicy();
+    //여기에서 할인 정책을 바꾸기 위해서는 OCP원칙이 위배된당
+
+    private DiscountPolicy discountPolicy;// 이렇게 작성하면 구체화에 의존하지 않고 추상화에 의존하기 때문에 DIP정책을 위반하지 않는다.
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
