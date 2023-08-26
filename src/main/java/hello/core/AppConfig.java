1eeefe80983b2792aa3865ec7e8d648a64487342
DAHEEKIM1 <66730012+DAHEEKIM1@users.noreply.github.com>
2023-08-26 22:33:29 +0900

    Section3 ing

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -13,7 +16,16 @@ public class AppConfig {
         return new MemberServiceImpl(new MemoryMemberRepository());
 
     }
+
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService(){
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
     }
 }
